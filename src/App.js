68ab3c723d2814db65cafed0b7a444d2ade08254
chirag.chaurasiya <chirag.chaurasiya@ambab.com>
2022-05-22 12:16:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
-import HomePage from "./route/HomePage/HomePage";
 
 export default function App() {
     return (
       <div>
           <Button variant='primary'>Button</Button>
-            <HomePage />
-            <Link to="/about">About</Link> 
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from "./App";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter , Routes, Route} from "react-router-dom";
-import HomePage from './route/HomePage/HomePage';
-import AboutPage from './route/AboutPage/AboutPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<App />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="about" element={<AboutPage />} />
       </Routes>
   </BrowserRouter>
 );
